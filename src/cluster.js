525f947040a1bd45ab095d6d069e8fe152bf6cd0
dm-mrtnvch <dmvshn@gmail.com>
2024-02-13 03:37:26 +0400

    feat: horizontal scaling

--- src/cluster.js
+++ src/cluster.js
@@ -1,27 +1,28 @@
-import cluster from 'src/cluster.js'
-import { cpus } from 'os'
-import process from 'process'
-import http from 'http'
-import { server as app } from './server.js'
+import { cpus } from 'os';
+import process from 'process';
+import { createServer } from 'http';
+import {server} from './server.js';
 
-const numCPUs = cpus().length
+const cluster = await import('cluster');
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`)
+const numCPUs = cpus().length;
 
-  let port = parseInt(process.env.PORT || '4000', 10)
+if (cluster.default.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  let port = parseInt(process.env.PORT || '4000', 10);
 
   for (let i = 0; i < numCPUs - 1; i++) {
-    let worker = cluster.fork();
-    worker.send({ workerPort: port + i })
+    let worker = cluster.default.fork();
+    worker.send({ workerPort: port + i });
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`)
-  })
+  cluster.default.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
 } else {
   process.on('message', (msg) => {
-    http.createServer(app).listen(msg.workerPort)
-    console.log(`Worker ${process.pid} started on port ${msg.workerPort}`)
-  })
+    createServer(server).listen(msg.workerPort);
+    console.log(`Worker ${process.pid} started on port ${msg.workerPort}`);
+  });
 }
